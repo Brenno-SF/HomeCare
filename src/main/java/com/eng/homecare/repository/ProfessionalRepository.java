9024eef3614690c56a94b7da3ffe90db3cfd9f03
Estêvão <sousaestevao17@gmail.com>
2025-06-02 17:28:52 -0300

    add: Professional Repository
    armazenamento de objetos

---
+++ src/main/java/com/eng/homecare/repository/ProfessionalRepository.java
@@ -0,0 +1,8 @@
+package com.eng.homecare.repository;
+
+import com.eng.homecare.entities.Professional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessionalRepository extends JpaRepository<Professional, Long> {
+
+}
