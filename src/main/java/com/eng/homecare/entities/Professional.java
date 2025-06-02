84f9aa6ba596e6089809c49791466c29896e4938
Estêvão <sousaestevao17@gmail.com>
2025-06-02 11:56:20 -0300

    add: Class Professional
    Classe do usuário do tipo Profissional

---
+++ src/main/java/com/eng/homecare/entities/Professional.java
@@ -0,0 +1,29 @@
+package com.eng.homecare.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "professional_tb")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Professional {
+
+    @Id
+    @Column(name = "id_fk")
+    private Long professionalId;
+
+    @MapsId
+    @JoinColumn(name = "id_fk")
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(name = "bio", length = 500, nullable = true)
+    private String bio;
+
+    @Column(name = "description", length = 500, nullable = true)
+    private String description;
+
+    @Column(name = "rate")
+    private int rate;
+}
